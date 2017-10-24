869bee2d2aae7a300b6225f61b4c5d9a5b1a5d19
Pratik <pratikoct15@yahoo.co.in>
2017-10-23 22:00:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 
-def function(x):
+
+def fizzbuzz(x):
     if x % 3 == 0 and x % 5 == 0:
-        print("divisible by both")
+        print('fizzbuzz')
     elif x % 3 == 0:
-        print("fish")
+        print('fizz')
     elif x % 5 == 0:
-        print("buzz")
+        print('buzz')
     else:
-        print("Number not divisble by 3 or 5")
-    
-function(int(input("Enter the number")))
+        print('Not divisble by 3 or 5')
+
+def loop_it(x):
+    # check if list or int
+    # we check if a comma is in x to see if its a list
+    if ',' in x:
+        print('list')
+        # turn to list
+        x = x.split(',')
+        for el in x:
+            # turn to int
+            el = int(el)
+            fizzbuzz(el)
+    else:
+        x = int(x)
+        fizzbuzz(x)
+        
+num = input('Enter a single number or a list of numbers: ')
+loop_it(num)
